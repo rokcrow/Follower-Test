5cbfcbfdda2486680e5e350a6615cb89ac705069
Alexis Hevia <hevia.alexis@gmail.com>
2012-05-26 22:03:53 -0500

    data object loading dynamically

--- game.js
+++ game.js
@@ -2,24 +2,31 @@ window.Game = new function(){
 	this.username = '';
 	this.data = [ 
 		{
-			user: { id: 1234565, username: 'alexishevia' },
+			user: { id: 1, username: 'alexishevia' },
 			tweets: [
-				{ id: 206435331296342000, text: "Este es un tweet de mentira" },
-				{ id: 206435331296342354, text: "Follower Test is the shiznit!" },
+				{ id: 11, text: "Este es un tweet de mentira" },
+				{ id: 12, text: "Follower Test is the shiznit!" },
 			]
 		},
 		{
-			user: { id: 7834527, username: 'walexis' },
+			user: { id: 2, username: 'walexis' },
 			tweets: [
-				{ id: 206435332126342100, text: "Yo soy el verdadero Alexis Hevia" },
-				{ id: 206435331296342354, text: "Follower Test sucks!" },
+				{ id: 21, text: "Yo soy el verdadero Alexis Hevia" },
+				{ id: 22, text: "Follower Test sucks!" },
+			]
+		},
+		{
+			user: { id: 3, username: 'rogelio' },
+			tweets: [
+				{ id: 31, text: "Yo no tengo nada que ver con los Alexos" },
+				{ id: 32, text: "Tengo otro tweet pa seguir la tradición" },
 			]
 		}
 	]
 	this.answers = [
-		{ user_id: 1234565, tweet_id: 206435331296342000 },
-		{ user_id: 1234565, tweet_id: 206435331296342000 },
-		{ user_id: 1234565, tweet_id: 206435331296342000 }
+		{ user_id: 1, tweet_id: 11 },
+		{ user_id: 2, tweet_id: 21 },
+		{ user_id: 3, tweet_id: 31 }
 	]
 
   this.start = function(username) {
@@ -28,12 +35,13 @@ window.Game = new function(){
     $.jsonp({
 			url: 'https://api.twitter.com/1/friends/ids.json?callback=?&screen_name=' + this.username,
 			success: function(data) {
+				window.data = data;
 				var user_ids = data.ids
 				var random_ids = that.getRandom(user_ids, 10);
-				that.random_ids = random_ids;
+				that.data = that.getData(random_ids);
       },
       error: function() {
-      	alert("There was an error. We couldn't find the people you follow");
+      	alert("There was an error. We couldn't find your 'following' list.");
       }
 		});
 	};
@@ -41,6 +49,7 @@ window.Game = new function(){
 	this.getRandom = function (user_ids, cant) {
     var length = user_ids.length;
     var numbers = [];
+    var ids = [];
     for (i = 0; i < cant; i++){
         do {
             var n = Math.floor((Math.random()*length)+1); 
@@ -51,16 +60,53 @@ window.Game = new function(){
                 }
             }
         }while (found);
+        ids.push(user_ids[n]);
         numbers.push(n);
     }
-    return numbers;
+    return ids;
   };
 		
-	this.getTweets = function(user_ids) {
-		for(id in user_ids) {
+	this.getData = function(user_ids) {
+		var data = [];
+		$.each(user_ids, function(index, user_id) {
+			var user_data = { user: {}, tweets: [] }
+			var url = 'https://api.twitter.com/1/statuses/user_timeline.json?callback=?&count=10&user_id=' + user_id;
+			console.log(url);
 			$.jsonp({
-				url: 'https://api.twitter.com/1/statuses/user_timeline.json?&count=10&user_id=' + id
-			})
-		}
+				url: url,
+				success: function(result) {
+					console.log('user_timeline success');
+					console.log(result);
+					user_data.user = {id: result[0].user.id, username: result[0].user.screen_name}
+					$.each(result, function(index, tweet) {
+						user_data.tweets.push({id: tweet.id, text: tweet.text});
+					});
+					data.push(user_data);
+				},
+				error: function(xhr) {
+					console.log('user_timeline error');
+				}
+			});
+		});
+		return data;
 	};
+	
+	this.grade = function (data, answers) {
+    var correct_ans = 0;
+    $.each(answers, function(i, answer) {
+      $.each(data, function(j, person) {
+        if (person.user.id == answer.user_id) {
+          $.each(person.tweets, function(k, tweet) {
+            if(answer.tweet_id == tweet.id) {
+              correct_ans++;
+            }
+          });
+        }
+      });
+    });
+    return correct_ans;
+  }
+  
+  this.grade(this.data, this.answers);
+
 }
